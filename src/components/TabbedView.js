6bef356c10023197f928f3e697ee3d894cfa30bd
dansut24 <Danieljamessutton18@outlook.com>
2025-04-10 06:58:51 +0100

    Add files via upload

--- src/components/TabbedView.js
+++ src/components/TabbedView.js
@@ -192,7 +192,9 @@ if (savedQuery) {
   
     setSocket(newSocket);
   
-    return () => {
+    return (
+  <>
+  ) => {
       newSocket.disconnect();
     };
   }, []);
@@ -898,13 +900,13 @@ if (savedQuery) {
     }}
     showLabels
   >
-    <BottomNavigationAction label="Dashboard" value="Dashboard" icon={<HomeIcon />
-    <BottomNavigationAction label="Incidents" value="Incidents" icon={<FolderIcon />
-    <BottomNavigationAction label="Requests" value="Service Requests" icon={<BuildIcon />
-    
-    <BottomNavigationAction label="Changes" value="Changes" icon={<BuildIcon />
-    
-    <BottomNavigationAction label="More" value="More" icon={<MoreVertIcon />
+    <BottomNavigationAction label="Dashboard" value="Dashboard" icon={<HomeIcon />} sx={{ flexShrink: 0, minWidth: 0 }} />
+    <BottomNavigationAction label="Incidents" value="Incidents" icon={<FolderIcon />} sx={{ flexShrink: 0, minWidth: 0 }} />
+    <BottomNavigationAction label="Requests" value="Service Requests" icon={<BuildIcon />} sx={{ flexShrink: 0, minWidth: 0 }} />
+    } sx={{ flexShrink: 0, minWidth: 0 }} />
+    <BottomNavigationAction label="Changes" value="Changes" icon={<BuildIcon />} sx={{ flexShrink: 0, minWidth: 0 }} />
+    } sx={{ flexShrink: 0, minWidth: 0 }} />
+    <BottomNavigationAction label="More" value="More" icon={<MoreVertIcon />} sx={{ flexShrink: 0, minWidth: 0 }} />
   </BottomNavigation>
 </Box>
 
