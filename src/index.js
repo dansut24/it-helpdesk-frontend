71a85610eefb978566ed5ce3dfb4245bd362597e
dansut24 <Danieljamessutton18@outlook.com>
2025-04-14 23:36:57 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import LandingPage from './pages/LandingPage';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const isTenant = window.location.hostname.includes('-itsm.');
+
+if (isTenant) {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <LandingPage />
+    </React.StrictMode>
+  );
+}
