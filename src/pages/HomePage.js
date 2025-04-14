4e20d4117944966bccbfe38277e80611b78d0f77
dansut24 <Danieljamessutton18@outlook.com>
2025-04-14 23:52:09 +0100

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => (
+  <div style={ padding: '2rem' }>
+    <h1>Home Page</h1>
+    <p>Welcome to the home section of Hi5Tek.</p>
+    
+    
+  </div>
+);
+
+export default HomePage;
