6777252c42405605d8227089e8e7d8fb20f461ba
dansut24 <Danieljamessutton18@outlook.com>
2025-04-14 23:45:45 +0100

    Add files via upload

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,13 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
     <div style={{ textAlign: 'center', padding: '4rem' }}>
       <h1>Welcome to Hi5Tek</h1>
       <p>The modern ITSM platform for teams who move fast.</p>
-      <a href="/signup" style={{
+      <Link to="/signup" style={{
         marginTop: '2rem',
         padding: '1rem 2rem',
         backgroundColor: '#2D9CDB',
@@ -16,7 +17,7 @@ const LandingPage = () => {
         fontSize: '1.2rem'
       }}>
         Get Started Free
-      </a>
+      </Link>
     </div>
   );
 };
