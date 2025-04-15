07058f5f6cf427a9a55383f8e845c1d5c614f77f
dansut24 <Danieljamessutton18@outlook.com>
2025-04-15 21:25:11 +0100

    Add files via upload

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
+import SignupPage from "./SignupPage";
+import DemoPage from "./DemoPage";
+
+const Home = () => (
+  <>
+    <Hero />
+    <Features />
+  </>
+);
 
 const LandingPage = () => {
   return (
     <>
-      <Hero />
-      <Features />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/demo" element={<DemoPage />} />
+      </Routes>
       <Footer />
     </>
   );
