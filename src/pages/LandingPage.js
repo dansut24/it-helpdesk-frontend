0d727923a3082100689110e5650892deb28bd91b
dansut24 <Danieljamessutton18@outlook.com>
2025-04-15 21:16:03 +0100

    Add files via upload

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,29 +1,13 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Signup from "./Signup";
-import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
 import Footer from "../components/Footer";
-
-const Home = () => (
-  <div style={{ padding: "2rem" }}>
-    <h1>Welcome to Hi5Tech</h1>
-    <p>Modern tools for growing teams.</p>
-    <Link to="/signup">
-      <button style={{ marginTop: "1rem" }}>Get Started Free</button>
-    </Link>
-  </div>
-);
 
 const LandingPage = () => {
   return (
     <>
-      <Header />
-      <main style={{ minHeight: "80vh" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </main>
+      <Hero />
+      <Features />
       <Footer />
     </>
   );
