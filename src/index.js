fe9d54e84dcf6f328798bd94d7c425c12a15eb2d
dansut24 <Danieljamessutton18@outlook.com>
2025-04-15 20:38:52 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import LandingPage from './pages/LandingPage';
+import { AuthProvider } from './context/AuthContext';
+import { TabProvider } from './context/TabContext';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 const isTenant = window.location.hostname.includes('-itsm.');
 
 if (isTenant) {
   root.render(
     <React.StrictMode>
-      <App />
+      <AuthProvider>
+        <TabProvider>
+          <App />
+        </TabProvider>
+      </AuthProvider>
     </React.StrictMode>
   );
 } else {
