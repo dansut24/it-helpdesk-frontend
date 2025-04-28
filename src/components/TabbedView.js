424d02833bf958538b99f96a10175f77df40863a
dansut24 <Danieljamessutton18@outlook.com>
2025-04-28 10:49:22 +0100

    Update TabbedView.js

--- src/components/TabbedView.js
+++ src/components/TabbedView.js
@@ -570,7 +570,7 @@ const HoverMenuItem = ({ icon, label, active, open, subItems }) => {
 
 return (
   <>
-<Box sx={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+<Box sx={{ display: 'flex', minHeight: '100vh' }}>
       {/* Sidebar */}
  {/* Sidebar */}
 {isMobile ? (
